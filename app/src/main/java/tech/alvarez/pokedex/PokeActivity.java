9ef1c7b5eadffe48cd34d2237b06b48c74694157
Paola <sm13ma.paolacorral@gmail.com>
2019-03-28 09:50:12 -0600

    La tercera es la vencida

--- app/src/main/java/tech/alvarez/pokedex/PokeActivity.java
+++ app/src/main/java/tech/alvarez/pokedex/PokeActivity.java
@@ -1,4 +1,18 @@
 package tech.alvarez.pokedex;
 
-public class PokeActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class PokeActivity extends AppCompatActivity{
+
+    private static final String TAG = "SomeActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: called");
+    }
+
 }
